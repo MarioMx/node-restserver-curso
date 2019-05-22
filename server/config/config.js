2960e4fe19ba172fd3708f95e34fa7809235b397
mario.marquez <mario.marquez@INFOFUENTEVILLA>
2019-05-22 12:04:09 -0500

    variable de entorno a MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 //if (process.env.Node_EMV === 'dev') {
 //    urlDB = 'mongodb://localhost:27017/cafe'
 //} else {
-urlDB = 'mongodb+srv://MarioMx:Mam153615!@cluster0-wdfkv.mongodb.net/cafe'
+urlDB = process.env.MONGO_URI
     //}
 
 process.env.urlDB = urlDB;
